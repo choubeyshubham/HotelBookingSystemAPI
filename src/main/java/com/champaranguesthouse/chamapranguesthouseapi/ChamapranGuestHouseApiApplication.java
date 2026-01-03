43543588d3a7626a9543b7346d9a1c20b00d3d4b
choubeyshubham <shubhamchoubey96@gmail.com>
2026-01-03 20:24:52 +0530

    01 commit - Creating Project

---
+++ src/main/java/com/champaranguesthouse/chamapranguesthouseapi/ChamapranGuestHouseApiApplication.java
@@ -0,0 +1,13 @@
+package com.champaranguesthouse.chamapranguesthouseapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChamapranGuestHouseApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChamapranGuestHouseApiApplication.class, args);
+    }
+
+}
